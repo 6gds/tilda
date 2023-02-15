88092988a5eb79de408f903e7bda6ba8925695b5
Denis Gevorkyan <denisgevor200@gmail.com>
2023-02-15 22:58:15 +0300

    исправление 2-го задания

--- Задание2/zadanie2.php
+++ Задание2/zadanie2.php
@@ -3,22 +3,53 @@
 $numbers = [];
 $notUniqueNumbers = [];
 
+$sumInRow = [];
+$sumInColumn = [];
+
 for ($i = 0; $i < 5; $i++) {
     for ($j = 0; $j < 7; $j++) {
         //можно do while использовать покрасивше будет
-        while (in_array($randNumber = rand(1, 1000), $notUniqueNumbers)) {
-            $randNumber = rand(1, 1000);
+        while (in_array($randNumber = rand(1, 66), $notUniqueNumbers)) {
+            $randNumber = rand(1, 66);
         };
-
+        
         $numbers[$i][$j] = $randNumber;
+
+        if (empty($sumInRow[$i])) {
+            $sumInRow[$i] = $randNumber;
+        } else {
+            $sumInRow[$i] += $randNumber;
+        }
+
+        if (empty($sumInColumn[$j])) {
+            $sumInColumn[$j] = $randNumber;
+        } else {
+            $sumInColumn[$j] += $randNumber;
+        }
     }
 }
 
 echo "</br>";
 echo "</br>";
 
-foreach ($numbers as $numberRow) {
-    foreach ($numberRow as $number) {
-        echo "$number ";
+echo "Cумма в столбце";
+echo "</br>";
+echo "</br>";
+
+for ($j = 0; $j < 7; $j++) {
+    echo $sumInColumn[$j] . " ";
+}
+
+echo "</br>";
+echo "</br>";
+
+for ($i = 0; $i < 5; $i++) {
+    for ($j = 0; $j < 7; $j++) {
+        echo $numbers[$i][$j] . " ";
     }
+
+    echo "Cумма в строке - " . $sumInRow[$i];
+
+    echo "</br>";
+    echo "</br>";
 }
